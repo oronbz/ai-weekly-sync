d9d6203b02c3b108252e546e22fe1ed5711245e5
Oron Ben Zvi <oronbz@gmail.com>
2026-02-01 13:59:45 +0200

    Add first sync

--- src/components/MarkdownRenderer.jsx
+++ src/components/MarkdownRenderer.jsx
@@ -1,10 +1,16 @@
 import ReactMarkdown from "react-markdown";
 import remarkGfm from "remark-gfm";
+import rehypeExternalLinks from "rehype-external-links";
 
 function MarkdownRenderer({ content }) {
   return (
     <div className="markdown-content">
-      <ReactMarkdown remarkPlugins={[remarkGfm]}>{content}</ReactMarkdown>
+      <ReactMarkdown
+        remarkPlugins={[remarkGfm]}
+        rehypePlugins={[[rehypeExternalLinks, { target: "_blank" }]]}
+      >
+        {content}
+      </ReactMarkdown>
     </div>
   );
 }
